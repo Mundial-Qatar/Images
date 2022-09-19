d2e6f3724ec42c7b48a93138b22be888dd803be9
Mundial-Qatar <113921123+Mundial-Qatar@users.noreply.github.com>
2022-09-19 12:07:09 -0300

    Add files via upload

--- auth_google.py
+++ auth_google.py
@@ -6,11 +6,12 @@ import asyncio
 # https://frankie567.github.io/httpx-oauth/oauth2/
 from httpx_oauth.clients.google import GoogleOAuth2
 
-
+print('Corre el archivo Auth')
 #Video youtube: https://www.youtube.com/watch?v=WKnnHbS104A
 # github repo: https://github.com/rsarosh/streamlit
 # aca me meti para configurar mi cuenta desarrollador de google https://console.cloud.google.com/apis/credentials?project=proyectostreamlit
 
+Pag_luego_login = ''
 
 if os.path.isfile('my_secrets.py'): #Con esto chequeo si estoy en entorno local
 	import my_secrets as ms
@@ -41,10 +42,11 @@ async def get_email(client: GoogleOAuth2, token: str):
     user_id, user_email = await client.get_id_email(token)
     return user_id, user_email
 
+
 def get_login_str():
     client: GoogleOAuth2 = GoogleOAuth2(CLIENT_ID, CLIENT_SECRET)
     authorization_url = asyncio.run(
-        get_authorization_url(client, REDIRECT_URI))
+        get_authorization_url(client, REDIRECT_URI+Pag_luego_login))
     return f"""<a target = '{authorization_url}' href = '{authorization_url}'> Google login </a >"""
 
 def display_user() -> void:
