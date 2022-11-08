4ad28d33c45ea83f9f8550bad93fb085dff88322
Mundial-Qatar <113921123+Mundial-Qatar@users.noreply.github.com>
2022-11-08 18:58:42 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import time, datetime
 from deta import Deta
 from auth_google import *
 import asyncio
+import altair as alt
 
 
 # from streamlit_option_menu import option_menu
@@ -43,6 +44,8 @@ db = deta.Base("primera_fase_bwin")
 db_r = deta.Base("resultados")
 db_log = deta.Base("usuarios_db")
 db_s = deta.Base("semi")
+db_puntos = deta.Base("tabla_puntos")
+db_torneo = deta.Base("torneo_amigos")
 
 def fetch_df(db):
 	"""Devuelve la info de la base de datos"""
@@ -58,7 +61,6 @@ df_r = fetch_df(db_r)
 df_r = pd.DataFrame.from_dict(df_r)
 df_s = fetch_df(db_s)
 df_s = pd.DataFrame.from_dict(df_s)
-
 
 def set_user():
 	st.session_state['usuario'] = display_user()
@@ -116,7 +118,7 @@ col_a , col_b = st.columns([10,4])
 with col_a:
 	st.title('Prode Qatar 2022')
 with col_b:
-	seccion = st.selectbox('Ronda',('Fase de grupos','Semifinalistas','Simulador'),key='params_key') #
+	seccion = st.selectbox('Ronda',('Fase de grupos','Semifinalistas','Simulador','Posiciones','Torneo de amigos'),key='params_key') #
 
 st.markdown(body =
  						 f"""
@@ -131,104 +133,148 @@ if Corrio == 'OK':
 	####----Cálculos que quiero hacer para este usuario-----------------------------------------------------------
 	
 	#st.session_state['usuario']
-	puntos_player = 0
-	dicc = {}
-	for i in range(0,8):
-		dicc[grupos[i]] = df[df['grupo'] == grupos[i]]
-		for j in range(0,len(dicc[grupos[i]])):
-			key_a = str(i)+'-'+str(j)+'a'
-			key_b = str(i)+'-'+str(j)+'b'
-			rec_a = 0
-			rec_b = 0
-			try: #Este try es por si la base de datos de resultados está vacía. En ese caso df_r['usuario'] falla
-				if st.session_state['usuario'] in [x for x in df_r['usuario']]:
-					max_fecha = df_r[(df_r['usuario'] == st.session_state['usuario'])]['fecha'].max()
-					row = df_r[(df_r['usuario'] == st.session_state['usuario'])&(df_r['fecha'] == max_fecha)]
+	def puntos_segun_fecha_usuario(fecha='2022-12-18',usuario=st.session_state['usuario']):
+		fecha_parametro_date = datetime.strptime(fecha, '%Y-%m-%d')
+		puntos_player = 0
+		dicc = {}
+		for i in range(0,8):
+			dicc[grupos[i]] = df[df['grupo'] == grupos[i]]
+			for j in range(0,len(dicc[grupos[i]])):
+				key_a = str(i)+'-'+str(j)+'a'
+				key_b = str(i)+'-'+str(j)+'b'
+				rec_a = 0
+				rec_b = 0
+				try: #Este try es por si la base de datos de resultados está vacía. En ese caso df_r['usuario'] falla
+					if usuario in [x for x in df_r['usuario']]:
+						max_fecha = df_r[(df_r['usuario'] == usuario)]['fecha'].max()
+						row = df_r[(df_r['usuario'] == usuario)&(df_r['fecha'] == max_fecha)]
+						rec_a = [x for x in row[key_a]][0]
+						rec_b = [x for x in row[key_b]][0]
+				except:
+					pass
+				fecha_partido_str_hora = dicc[grupos[i]]['Fecha_partido'][j]
+				#fecha_partido_str_hora = dicc[grupos[0]]['Fecha_partido'][0]
+				fecha_partido_date_hora = datetime.strptime(fecha_partido_str_hora, '%Y-%m-%d %H:%M:%S')
+				fecha_partido_str = fecha_partido_date_hora.strftime('%Y-%m-%d')
+				fecha_partido_date = datetime.strptime(fecha_partido_str, '%Y-%m-%d')
+				
+				key_a = str(i)+'-'+str(j)+'a'
+				key_b = str(i)+'-'+str(j)+'b'
+				rec_a_real = 0
+				rec_b_real = 0
+				try: #Este try es por si la base de datos de resultados está vacía. En ese caso df_r['usuario'] falla
+					max_fecha = df_r[(df_r['usuario'] == 'mazzafedeitalia@gmail.com')]['fecha'].max()
+					row_real = df_r[(df_r['usuario'] == 'mazzafedeitalia@gmail.com')&(df_r['fecha'] == max_fecha)]
+					rec_a_real = [x for x in row_real[key_a]][0]
+					rec_b_real = [x for x in row_real[key_b]][0]
+				except:
+					pass
+				
+				rec_a = 0
+				rec_b = 0
+				try: #Este try es por si la base de datos de resultados está vacía. En ese caso df_r['usuario'] falla
+					max_fecha = df_r[(df_r['usuario'] == usuario)]['fecha'].max()
+					row = df_r[(df_r['usuario'] == usuario)&(df_r['fecha'] == max_fecha)]
 					rec_a = [x for x in row[key_a]][0]
 					rec_b = [x for x in row[key_b]][0]
-			except:
-				pass
-			text_hover = dicc[grupos[i]]['Fecha_partido'][j]			
-			key_a = str(i)+'-'+str(j)+'a'
-			key_b = str(i)+'-'+str(j)+'b'
-			rec_a_real = 0
-			rec_b_real = 0
-			try: #Este try es por si la base de datos de resultados está vacía. En ese caso df_r['usuario'] falla
-				max_fecha = df_r[(df_r['usuario'] == 'mazzafedeitalia@gmail.com')]['fecha'].max()
-				row_real = df_r[(df_r['usuario'] == 'mazzafedeitalia@gmail.com')&(df_r['fecha'] == max_fecha)]
-				rec_a_real = [x for x in row_real[key_a]][0]
-				rec_b_real = [x for x in row_real[key_b]][0]
-			except:
-				pass
-			
-			rec_a = 0
-			rec_b = 0
-			try: #Este try es por si la base de datos de resultados está vacía. En ese caso df_r['usuario'] falla
-				max_fecha = df_r[(df_r['usuario'] == st.session_state['usuario'])]['fecha'].max()
-				row = df_r[(df_r['usuario'] == st.session_state['usuario'])&(df_r['fecha'] == max_fecha)]
-				rec_a = [x for x in row[key_a]][0]
-				rec_b = [x for x in row[key_b]][0]
-			except:
-				pass						
-			
-			
-			fecha_formato_fecha = datetime.strptime(text_hover, '%Y-%m-%d %H:%M:%S')
-
-			puntos_ganador = 0
-			puntos_marcador = 0
-			puntos_partido = 0
-			
-			formato_ganador = 'class-pendiente'
-			formato_marcador = 'class-pendiente'
-			formato_total = 'class-pendiente'
-
-			if rec_a_real == 99:
-				rec_a_real = 'X'
-				rec_b_real = 'X'
-				estado_resultado = 'Todavía no se cargó el resultado'
-			else:
-				if rec_a_real > rec_b_real:
-					outcome_real = 'Gana A'
-				elif rec_a_real < rec_b_real:
-					outcome_real = 'Gana B'
-				else:
-					outcome_real = 'Empate'
+				except:
+					pass						
 				
-				if rec_a > rec_b:
-					outcome_pronostico = 'Gana A'
-					ratio_ganador = float(dicc[grupos[i]]['win_a'][j])
-				elif rec_a < rec_b:
-					outcome_pronostico = 'Gana B'
-					ratio_ganador = float(dicc[grupos[i]]['win_b'][j])
-				else:
-					outcome_pronostico = 'Empate'
-					ratio_ganador = float(dicc[grupos[i]]['tie'][j])
+				if fecha_partido_date <= fecha_parametro_date:
 				
-				if outcome_real == outcome_pronostico:
-					puntos_ganador = ratio_ganador
-					formato_ganador = 'class-ganador'
-					puntos_partido = puntos_ganador
-				else:
 					puntos_ganador = 0
-					formato_ganador = 'class-perdedor'
-					formato_marcador = 'class-perdedor'
-				
-				if (rec_a == rec_a_real) & (rec_b == rec_b_real):
-					if rec_a + rec_b >2.5:
-						puntos_partido = round(2*puntos_ganador,3)
-						formato_marcador = 'class-ganador'
-					else:
-						puntos_partido = round(1.5*puntos_ganador,3)
-						formato_marcador = 'class-ganador'
-					puntos_marcador = round(puntos_partido - puntos_ganador,3)
-				else:
 					puntos_marcador = 0
-				
-				puntos_player = puntos_player + puntos_partido
-				st.session_state['puntos_player'] = puntos_player
-				estado_resultado = 'Resultado disponible'
+					puntos_partido = 0
+					
+					formato_ganador = 'class-pendiente'
+					formato_marcador = 'class-pendiente'
+					formato_total = 'class-pendiente'
+		
+					if rec_a_real == 99:
+						rec_a_real = 'X'
+						rec_b_real = 'X'
+						estado_resultado = 'Todavía no se cargó el resultado'
+					else:
+						if rec_a_real > rec_b_real:
+							outcome_real = 'Gana A'
+						elif rec_a_real < rec_b_real:
+							outcome_real = 'Gana B'
+						else:
+							outcome_real = 'Empate'
+						
+						if rec_a > rec_b:
+							outcome_pronostico = 'Gana A'
+							ratio_ganador = float(dicc[grupos[i]]['win_a'][j])
+						elif rec_a < rec_b:
+							outcome_pronostico = 'Gana B'
+							ratio_ganador = float(dicc[grupos[i]]['win_b'][j])
+						else:
+							outcome_pronostico = 'Empate'
+							ratio_ganador = float(dicc[grupos[i]]['tie'][j])
+						
+						if outcome_real == outcome_pronostico:
+							puntos_ganador = ratio_ganador
+							formato_ganador = 'class-ganador'
+							puntos_partido = puntos_ganador
+						else:
+							puntos_ganador = 0
+							formato_ganador = 'class-perdedor'
+							formato_marcador = 'class-perdedor'
+						
+						if (rec_a == rec_a_real) & (rec_b == rec_b_real):
+							if rec_a + rec_b >2.5:
+								puntos_partido = round(2*puntos_ganador,3)
+								formato_marcador = 'class-ganador'
+							else:
+								puntos_partido = round(1.5*puntos_ganador,3)
+								formato_marcador = 'class-ganador'
+							puntos_marcador = round(puntos_partido - puntos_ganador,3)
+						else:
+							puntos_marcador = 0
+						
+						puntos_player = puntos_player + puntos_partido
+						
+		return puntos_player
+				 
 	
+	
+	st.session_state['puntos_player'] = puntos_segun_fecha_usuario()
+	
+	
+	
+	####----Función que evalua resultados-----------------------------------------------------------
+	
+	def función_resultados_totales():
+		fechas_string = ['2022-11-20','2022-11-21','2022-11-22','2022-11-23','2022-11-24',
+					 '2022-11-25','2022-11-26','2022-11-27','2022-11-28','2022-11-29',
+					 '2022-11-30','2022-12-01','2022-12-02']
+# 						,'2022-12-03','2022-12-04',
+# 					 '2022-12-05','2022-12-06','2022-12-07','2022-12-08','2022-12-09',
+# 					 '2022-12-10','2022-12-11','2022-12-12','2022-12-13','2022-12-14',
+# 					 '2022-12-15','2022-12-16','2022-12-17','2022-12-18',]
+		
+		usuarios_con_pronosticos = df_r['usuario'].unique()
+		def pasar_a_fecha(fecha):
+			return datetime.strptime(fecha, '%Y-%m-%d')
+		
+		df_pnts = pd.DataFrame(columns=['user','fecha', 'puntos'])
+		for user in usuarios_con_pronosticos:
+			for fecha in fechas_string:
+				puntos = puntos_segun_fecha_usuario(fecha=fecha,usuario=user)
+				#puntos = 92
+				df_puntos_append = pd.DataFrame({'user':user,'fecha':fecha,'puntos':puntos}, index=[0])	
+				df_pnts = df_pnts.append(df_puntos_append)
+		df_pnts_dict = df_pnts.to_dict('list')
+		return db_puntos.put({'df_puntos': df_pnts_dict,'fecha':datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')})
+	
+
+
 	####---------------------------------------------------------------
+	
+		
+	
+	
+	
 	
 	if seccion == 'Fase de grupos':
 		if (st.session_state['usuario'] != 'mazzafedeitalia@gmail.com')&(st.session_state['usuario'] != 'mazzafede@gmail.com'):
@@ -238,7 +284,11 @@ if Corrio == 'OK':
 		def subir_resultados():
 			st.session_state['fecha']=str(datetime.utcnow().date())+'-'+str(datetime.utcnow().time())
 			st.balloons()
+			if (st.session_state['usuario'] == 'mazzafedeitalia@gmail.com'):
+				función_resultados_totales()    #acá quiero que corra una función que evalúa resultados para todos
+				print('esto corrio')
 			return db_r.put({k: v for k, v in st.session_state.items()})
+		print('hay cartel?')
 		col_usuario, col_guardar = st.columns([1,1])
 		with col_usuario:
 			st.write('Hola '+st.session_state['usuario']+'!')
@@ -700,6 +750,133 @@ if Corrio == 'OK':
 			 						 """
 			 						 , unsafe_allow_html=True)
 		
-		st.session_state['semis'] = options		
+		st.session_state['semis'] = options
+		
+		
+		
+####-----Posiciones----------------------------------------------------------------------------------------------------------------
+					
+		
+	elif seccion == 'Posiciones':
+		df_torneo = fetch_df(db_torneo)
+		df_torneo = pd.DataFrame.from_dict(df_torneo)
+		mis_torneos = []
+		for i in range(0,len(df_torneo)):
+			if st.session_state['usuario'] in df_torneo.iloc[i]['friends_list']:
+				mis_torneos.append(df_torneo.iloc[i]['friends_name'])
+		mis_torneos.append('No estoy en ningún torneo')
+		friends_name = st.selectbox('Si estás en algún torneo elegilo',options=mis_torneos)
+		max_fecha = df_torneo[df_torneo['friends_name']==friends_name]['fecha'].max()
+		torneo_existente = df_torneo[(df_torneo['friends_name']==friends_name)&(df_torneo['fecha']==max_fecha)]
+		participantes = list(torneo_existente['friends_list'])[0]
+	
+		def pasar_a_fecha_2(fecha):
+			return datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+		def pasar_a_fecha_3(fecha):
+			return datetime.strptime(fecha, '%Y-%m-%d')		
+		df_puntos = fetch_df(db_puntos)
+		max_fecha = pasar_a_fecha_2('2020-11-05 19:33:00')
+		for i in range(0,len(df_puntos)):
+			if pasar_a_fecha_2(df_puntos[i]['fecha']) > max_fecha:
+				max_fecha = pasar_a_fecha_2(df_puntos[i]['fecha'])
+				df_puntos_aux = df_puntos[i]['df_puntos']
+		print(max_fecha)
+		df_puntos = df_puntos_aux.copy()
+		df_puntos = pd.DataFrame.from_dict(df_puntos)
+		df_puntos = df_puntos[df_puntos['user'].isin(participantes)]
+		
+		df_puntos['puntos'] = df_puntos['puntos'].astype(float)
+		df_puntos['fecha_date'] = df_puntos['fecha'].apply(lambda x: pasar_a_fecha_3(x))
+		max_puntos = df_puntos['puntos'].max()
+		
+		selection = alt.selection(type='single', fields=['user'], bind='legend')
+		
+		graph = alt.Chart(df_puntos).mark_line().encode(
+		    #y = alt.Y('fecha',scale=alt.Scale(domain=(0.3, 0.7))),
+			x = alt.X('fecha'),
+		    y = alt.Y('puntos',scale=alt.Scale(domain=(0, max_puntos))),
+			#size='Winrate_budget_ratio',
+			#text = 'Team2',
+			#color=alt.Color('user', scale=None),
+			color = 'user',
+			#tooltip='Team2:N'
+		).properties(width=1700,height=600).add_selection(selection)
+		
+		
+		marks = alt.Chart(df_puntos).mark_circle(size=100,opacity=1).encode(
+		    #y = alt.Y('fecha',scale=alt.Scale(domain=(0.3, 0.7))),
+			x = alt.X('fecha'),
+		    y = alt.Y('puntos'),
+			#text = 'Team2',
+			#color=alt.Color('user', scale=None),
+			color = 'user:N',
+			#tooltip='Team2:N'
+		).properties(width=1700,height=600)
+		
+				
+		
+# 		import altair_viewer
+# 		altair_viewer.display(graph)
+		
+		st.altair_chart((graph+marks).interactive(), use_container_width=False)
+
+####------Torneo de amigos-------------------------------------------------------------------------------------------------------------------------------
+
+
+	elif seccion == 'Torneo de amigos':		
+		print('vuelve a correr la pagina torneo de amigos')
+		
+		df_torneo = fetch_df(db_torneo)
+		df_torneo = pd.DataFrame.from_dict(df_torneo)
+		def subir_torneo():
+			if friends_name in list(df_torneo['friends_name']):
+				max_fecha = df_torneo[df_torneo['friends_name']==friends_name]['fecha'].max()
+				torneo_existente = df_torneo[(df_torneo['friends_name']==friends_name)&(df_torneo['fecha']==max_fecha)]
+				owner = str(list(df_torneo[df_torneo['friends_name']==friends_name]['owner'])[0])			
+				if st.session_state['usuario'] == owner:
+					db_torneo.put({'friends_name':friends_name,'friends_list':friends_list,'owner':st.session_state['usuario'],'fecha':datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')})
+					st.balloons()
+				else:
+					st.error('El creador de este torneo es'+owner+'. tenés que ser el creador del torneo para poder editarlo')			
+			else:
+				db_torneo.put({'friends_name':friends_name,'friends_list':friends_list,'owner':st.session_state['usuario'],'fecha':datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')})
+				st.balloons()
+				
+		st.markdown(body = '<br>', unsafe_allow_html=True)		
+		st.button('Guardar cambios',on_click=subir_torneo, key=5)
+		
+		friends_name = st.text_input(label='Ponele nombre a tu torneo', value="", key='valor_input')
+
+		pre_list = ''
+		if 'valor_input' in st.session_state:
+			print('corre el if valor input')
+			print(df_torneo['friends_name'])
+			if friends_name in list(df_torneo['friends_name']):
+				print('corre el if friends name')
+				max_fecha = df_torneo[df_torneo['friends_name']==friends_name]['fecha'].max()
+				torneo_existente = df_torneo[(df_torneo['friends_name']==friends_name)&(df_torneo['fecha']==max_fecha)]
+				owner = str(list(df_torneo[df_torneo['friends_name']==friends_name]['owner'])[0])				
+				if st.session_state['usuario'] == owner:
+					print('corre el if owner')
+					st.session_state['friends'] = list(df_torneo[df_torneo['friends_name']==friends_name]['friends_list'])[0]
+					for i in st.session_state['friends']:
+						pre_list = (pre_list +' '+i).strip()
+
+				else:
+ 					st.error('Este nombre de torneo ya está usado por '+owner)
+		
+		friends = st.text_input(label='Arma tu equipo de amigos: Agregá las direcciones de mail con el que llenaron el prode separandolos entre sí con un espacio', value=pre_list)
+		friends_list = friends.strip().split(" ")
+		print(friends_list)
+		friends_to_choose = st.multiselect(label='Acá va a ir apareciendo la lista completa',options= friends_list,default= friends_list)#,default=friends_to_choose
+		
+		
+		st.markdown(body = '<br>', unsafe_allow_html=True)
+		st.markdown(body = '<br>', unsafe_allow_html=True)	
+		st.markdown(body = '<br>', unsafe_allow_html=True)	
+		st.markdown(body = '<br>', unsafe_allow_html=True)	
+		st.markdown(body = '<br>', unsafe_allow_html=True)	
+		st.markdown(body = '<br>', unsafe_allow_html=True)		
+		
 else:
 	st.write('Usuario es desconocido')  
